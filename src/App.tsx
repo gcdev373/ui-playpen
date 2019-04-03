b9d870dcd053a6b29a95ed967b6d8fb071a1fa5a
GCHQ-11 <gchq.developer.11@gmail.com>
2019-04-03 15:46:12 +0100

    useCallback for onSearch

--- src/App.tsx
+++ src/App.tsx
@@ -3,40 +3,41 @@ import React, { useCallback, useState } from 'react';
 import './App.css';
 
 
-import {Client, ApiResponse, RequestParams} from '@elastic/elasticsearch';
-const client = new Client ({node: 'http://localhost:9200' });
+import { Client, ApiResponse, RequestParams } from '@elastic/elasticsearch';
+const client = new Client({ node: 'http://localhost:9200' });
 
 const App: React.FunctionComponent = () => {
-  
+
   const [count, setCount] = useState<number>(0);
 
   const increment = useCallback(() =>
     setCount(count + 1)
     , [count, setCount])
 
-    const params: RequestParams.Search =
-        {
-          index: 'git-*',
-          body: { user: 'stroomdev66'}
-        }
+  const params: RequestParams.Search =
+  {
+    index: 'git-*',
+    body: { user: 'stroomdev66' }
+  }
+
+  const onSearch = useCallback(() => {
+    client.search(params).then((result: ApiResponse) => {
+      console.log("The results are in");
+      console.log(result.body.hits.hits);
+    })
+      .catch((err: Error) => {
+        console.log(err);
+      })
+  }, [client]);
 
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>That dev has committed {count} objects</p>
         {
         }
-        <button onClick={
-          client.search(params).then ((result : ApiResponse) => {
-            console.log ("The results are in");
-            console.log (result.body.hits.hits);
-          })
-          .catch ((err : Error) =>  {
-            console.log(err);
-          })
-         
-          }>Increment</button>
+        <button onClick={onSearch}>Increment</button>
       </header>
     </div>
   );
