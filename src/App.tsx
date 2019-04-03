ecc74df2446a4e24cfc5c34a877bc2684b026d10
GCHQ-11 <gchq.developer.11@gmail.com>
2019-04-03 11:54:45 +0100

    demo use of hooks and function components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React, { useCallback, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+/**
+ * You want a Function component rather than a Class component.
+ * Hooks essentially are there to retire the old class based component.
+ */
+const App: React.FunctionComponent = () => {
+  const [count, setCount] = useState<number>(0);
+
+  const increment = useCallback(() =>
+    setCount(count + 1)
+    , [count, setCount])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
           </a>
-        </header>
-      </div>
-    );
-  }
+        <p>The Count is {count}</p>
+        <button onClick={increment}>Increment</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
